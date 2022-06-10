3a17e9c91c1682bdf7f0b2722ab98b3c62bac333
Edgar Ulloa <ulloa.edgar0203@gmail.com>
2022-06-09 17:22:36 -0700

    finished phase 3

--- sql-practice/phase-3.sql
+++ sql-practice/phase-3.sql
@@ -20,12 +20,12 @@ values
 insert into customers (name, email, phone)
 values
 ("Monica", "monica@friends.show", 2222222222),
-("Pheobe", "phoebe@friends.show", 3333333333);
+("Phoebe", "phoebe@friends.show", 3333333333);
 
 -- Part 4
 update customers
 set points = points + 3
-where name = "Pheobe";
+where name = "Phoebe";
 
 insert into coffee_orders (is_redeemed)
 values
@@ -50,6 +50,118 @@ where name = "Monica";
 -- Part 7
 select points from customers
 where name = "Rachel";
+
+update customers
+set points = 0
+where name = "Rachel";
+
+insert into coffee_orders (is_redeemed)
+values
+(1);
+
+-- Part 8
+insert into customers (name, email)
+values
+("Joey", "joey@friends.show"),
+("Chandler", "chandler@friends.show"),
+("Ross", "ross@friends.show");
+
+-- Part 9
+insert into coffee_orders (is_redeemed)
+values
+(0), (0), (0), (0),
+(0), (0);
+
+update customers
+set points = points + 6
+where name = "Ross";
+
+-- Part 10
+insert into coffee_orders (is_redeemed)
+values
+(0), (0), (0);
+
+update customers
+set points = points + 3
+where name = "Monica";
+
+-- Part 11
+select points
+from customers
+where name = "Phoebe";
+
+insert into coffee_orders (is_redeemed)
+values
+(0);
+
+update customers
+set points = points + 1
+where name = "Phoebe";
+
+-- Part 12
+DELETE FROM coffee_orders
+WHERE id in (18, 19);
+
+update customers
+set points = points - 2
+where name = "Ross";
+
+-- Part 13
+insert into coffee_orders (is_redeemed)
+values
+(0), (0);
+
+update customers
+set points = points + 2
+where name = "Joey";
+
+-- Part 14
+select points
+from customers
+where name = "Monica";
+
+insert into coffee_orders (is_redeemed)
+values
+(1);
+
+update customers
+set points = points - 10
+where name = "Monica";
+
+-- Part 15
+delete from customers
+where name = "Chandler";
+
+-- Part 16
+select points
+from customers
+where name = "Ross";
+
+update customers
+set points = points + 1
+where name = "Ross";
+
+insert into coffee_orders (is_redeemed)
+values
+(0);
+
+-- Part 17
+select points
+from customers
+where name = "Joey";
+
+update customers
+set points = points + 1
+where name = "Joey";
+
+insert into coffee_orders (is_redeemed)
+values
+(0);
+
+-- Part 18
+update customers
+set email = "p_as_in_phoebe@friends.show"
+where name = "Phoebe";
 
 select * from customers;
 select * from coffee_orders;
